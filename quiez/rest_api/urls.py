55c4140b185e61cb4d7eca26b611b9afb9b1cc2f
Valery <valera071998@gmail.com>
2019-03-24 23:00:09 +0300

    [Add] Test creation view router.

--- quiez/rest_api/urls.py
+++ quiez/rest_api/urls.py
@@ -1,18 +1,17 @@
 from django.urls import path, include
 
-from rest_framework_swagger.views import get_swagger_view
-
 from .views.auth import Registration
+from .views.test import TestList
 
 
 urlpatterns = [
-    # docs
-    path('', get_swagger_view(title='QuiEz API')),
-
     # browsable API autharization
     path('auth-rest/', include('rest_framework.urls', namespace='rest_framework')),
     # authorization stuff provided by rest_auth
     path('auth/', include('rest_auth.urls')),
     # registration
     path('auth/register/', Registration.as_view()),
+
+    # test
+    path('test', TestList.as_view()),
 ]
