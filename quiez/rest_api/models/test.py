e53832f65374e16475ec7cc54c82acafc7cdb454
Valery <valera071998@gmail.com>
2019-04-06 17:46:41 +0300

    [Change] Test model. Feedback questions adding.

--- quiez/rest_api/models/test.py
+++ quiez/rest_api/models/test.py
@@ -5,18 +5,28 @@ from django.utils.timezone import localtime
 
 class Test(models.Model):
     """
-    Test class.
-
-    * link - string that uniquely identifies test instance and used for sharing with potential test participants.
+    Test model class.
     """
     id = models.AutoField(primary_key=True)
     date_creation = models.DateTimeField(null=False)
     name = models.CharField(max_length=150, null=True)
     description = models.CharField(max_length=250, null=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=False, related_name="tests")
 
     def save(self, *args, **kwargs):
-        if not self.date_creation:                  # automatically fill date_creation when save instance
-            self.date_creation = localtime()
+        if not self.pk:
+            if not self.date_creation:  # automatically fill date_creation when save instance
+                self.date_creation = localtime()
 
         super().save(*args, **kwargs)
+
+        # feedback questions binding
+        from .question import QuestionFeedback
+
+        try:
+            _qs_questions_feedback = QuestionFeedback.objects.all()
+            for question_feedback in _qs_questions_feedback:
+                self.questions_feedback.add(question_feedback)
+                question_feedback.save()
+        except QuestionFeedback.DoesNotExist:
+            pass
