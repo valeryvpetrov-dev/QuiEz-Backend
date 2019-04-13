3c702d7a00b88ff435cf026173b48b0e95475f53
Valery <valera071998@gmail.com>
2019-04-13 16:59:30 +0300

    [Add] Answer submission serializer classes.

--- quiez/rest_api/serializers/answer.py
+++ quiez/rest_api/serializers/answer.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 
-from ..models.answer import QuestionAnswer, QuestionFeedbackAnswer
+from ..models.answer import QuestionAnswer, QuestionFeedbackAnswer, \
+    QuestionAnswerSubmission, QuestionFeedbackAnswerSubmission
+from ..models.question import Question, QuestionFeedback
+from ..models.test import TestSubmission
 
 
 class QuestionAnswerPostSerializer(serializers.ModelSerializer):
@@ -33,4 +36,34 @@ class QuestionFeedbackAnswerGetSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = QuestionFeedbackAnswer
-        fields = ("id", "content", "is_right")
+        fields = ("id", "content")
+
+
+class QuestionAnswerSubmissionPostSerializer(serializers.ModelSerializer):
+    """
+    Question answer submission instance serializer class.
+
+    * Only for creation purposes.
+    """
+    test_submission = serializers.PrimaryKeyRelatedField(queryset=TestSubmission.objects.all())
+    question = serializers.PrimaryKeyRelatedField(queryset=Question.objects.all())
+    answer = serializers.PrimaryKeyRelatedField(queryset=QuestionAnswer.objects.all())
+
+    class Meta:
+        model = QuestionAnswerSubmission
+        fields = ('content', 'is_right', 'test_submission', 'question', 'answer')
+
+
+class QuestionFeedbackAnswerSubmissionPostSerializer(serializers.ModelSerializer):
+    """
+    Feedback question answer submission instance serializer class.
+
+    * Only for creation purposes.
+    """
+    test_submission = serializers.PrimaryKeyRelatedField(queryset=TestSubmission.objects.all())
+    question = serializers.PrimaryKeyRelatedField(queryset=QuestionFeedback.objects.all())
+    answer = serializers.PrimaryKeyRelatedField(queryset=QuestionFeedbackAnswer.objects.all())
+
+    class Meta:
+        model = QuestionFeedbackAnswerSubmission
+        fields = ('content', 'test_submission', 'question', 'answer')
